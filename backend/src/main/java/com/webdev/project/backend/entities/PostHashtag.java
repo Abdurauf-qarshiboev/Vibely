ebdc6566297bd1f01c65c427812b881db5aeb572
Ollayorbek <ollayorbek2005@gmail.com>
2025-04-05 11:58:04 +0500

    PostHashtag entity added

---
+++ backend/src/main/java/com/webdev/project/backend/entities/PostHashtag.java
@@ -0,0 +1,59 @@
+package com.webdev.project.backend.entities;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "post_hashtag")
+public class PostHashtag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "hashtag_id", nullable = false)
+    private Hashtag hashtag;
+
+    @Column(nullable = false, updatable = false)
+    private Timestamp createdAt;
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Hashtag getHashtag() {
+        return hashtag;
+    }
+
+    public void setHashtag(Hashtag hashtag) {
+        this.hashtag = hashtag;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
