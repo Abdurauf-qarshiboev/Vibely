6e5055795198e98d2e1a1c5def80ceedd084e2d0
Ollayorbek <ollayorbek2005@gmail.com>
2025-04-05 11:56:23 +0500

    Post entity updated

--- backend/src/main/java/com/webdev/project/backend/entities/Post.java
+++ backend/src/main/java/com/webdev/project/backend/entities/Post.java
@@ -6,99 +6,38 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.sql.Timestamp;
 import java.util.List;
 
+
 @Entity
 @Table(name = "posts")
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    @JsonIgnore
     private User user;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String title;
 
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    private Integer likeCount;
+    private Integer commentCount;
+
+    @Column(length = 255)
+    private String image;
+
+    private Boolean isPrivate;
+
+    @Column(nullable = false, updatable = false)
+    private Timestamp createdAt;
+
     @Column(nullable = false)
-    private String content;
+    private Timestamp updatedAt;
 
-    @CreationTimestamp
-    private Timestamp time;
-
-    private int likes;
-    private int reposts;
-    private int shares;
-
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Comment> comments;
-
-    public Post() {}
-
-    public Post(User user, String title, String content) {
-        this.user = user;
-        this.title = title;
-        this.content = content;
-        this.likes = 0;
-        this.reposts = 0;
-        this.shares = 0;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public Timestamp getTime() {
-        return time;
-    }
-
-    public int getLikes() {
-        return likes;
-    }
-
-    public int getReposts() {
-        return reposts;
-    }
-
-    public int getShares() {
-        return shares;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void addLike() {
-        this.likes++;
-    }
-
-    public void addRepost() {
-        this.reposts++;
-    }
-
-    public void addShare() {
-        this.shares++;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+    // Getters and Setters
 }
